ba11fd732a3bbefc88b3a649ec5422e0cb56a291
ciperuoy <long220352@icloud.com>
2024-12-08 15:41:34 -0500

    1.0.0
    deleted unecessary stuff from keyboardcollider.cs

--- Classes/KeyboardCollider.cs
+++ Classes/KeyboardCollider.cs
@@ -4,21 +4,8 @@ using static LegallyStupid.Menu.Main;
 
 namespace LegallyStupid.Classes
 {
-    public class KeyboardKey : MonoBehaviour
+	public class KeyboardKey : MonoBehaviour
 	{
 		public string key;
-		
-		public void OnTriggerEnter(Collider collider)
-		{
-			if ((collider == lKeyCollider || collider == rKeyCollider) && menu != null)
-			{
-				if (doButtonsVibrate)
-				{
-					GorillaTagger.Instance.StartVibration(collider == lKeyCollider, GorillaTagger.Instance.tagHapticStrength / 2f, GorillaTagger.Instance.tagHapticDuration / 2f);
-				}
-                GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(66, collider == lKeyCollider, buttonClickVolume / 10f);
-				PressKeyboardKey(key);
-            }
-		}
 	}
 }
